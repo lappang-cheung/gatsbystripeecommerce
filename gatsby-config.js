d950cadb5ec74c0c6b754a8436ac3cf2268a11b7
Leo Cheung <lappangcheung@gmail.com>
2020-05-18 20:04:59 -0400

    setup products

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Level Up Store`,
+    description: `Learning how to do e-commerce`,
+    author: `@lemonjai`,
   },
   plugins: [
     `gatsby-plugin-stripe`,
